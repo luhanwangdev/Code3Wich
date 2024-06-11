6b1af166d6617872dbcbef26eda76d8c37834ccc
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-06-11 13:30:16 +0800

    feat: set some utils and middlwares into server

---
+++ server/utils/appError.ts
@@ -0,0 +1,12 @@
+class AppError extends Error {
+  public statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this);
+  }
+}
+
+export default AppError;
