dc4abef8b4bfcc2da2a03eca4fc0ac64fdf26cc0
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-06-12 09:37:36 +0800

    delete: delete language selector

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -2,14 +2,12 @@ import { useState, useRef, useEffect } from "react";
 import { Box, Button, Flex } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
 import SideBar from "./SideBar.jsx";
-import LanguageSelector from "./LanguageSelector";
 import TabNavigation from "./TabNavigator";
 import { CODE_SNIPPETS } from "../constants";
 
 const CodeEditor = () => {
   const editorRef = useRef();
   const [value, setValue] = useState("");
-  const [language, setLanguage] = useState("javascript");
   const [activeFile, setActiveFile] = useState({});
   const [files, setFiles] = useState([]);
 
@@ -17,14 +15,6 @@ const CodeEditor = () => {
     editorRef.current = editor;
     editor.focus();
   };
-
-  // const onSelect = (activeFile) => {
-  //   // setLanguage(language);
-  //   console.log(activeFile);
-  //   console.log(activeFile.type);
-  //   console.log(CODE_SNIPPETS[activeFile.type]);
-  //   setValue(CODE_SNIPPETS[activeFile.type]);
-  // };
 
   useEffect(() => {
     const fetchFiles = async () => {
@@ -84,7 +74,6 @@ const CodeEditor = () => {
         />
       </Box>
       <Box flex="6">
-        {/* <LanguageSelector language={language} onSelect={onSelect} /> */}
         <TabNavigation
           files={files}
           activeFile={activeFile}
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,14 +1,5 @@
 import { faHtml5, faCss3, faJs } from "@fortawesome/free-brands-svg-icons";
 
-export const LANGUAGE_VERSIONS = {
-  javascript: "18.15.0",
-  typescript: "5.0.3",
-  python: "3.10.0",
-  java: "15.0.2",
-  csharp: "6.12.0",
-  php: "8.2.3",
-};
-
 export const CODE_SNIPPETS = {
   javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
   html: `html`,
