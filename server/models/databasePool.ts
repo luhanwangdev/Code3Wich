84a941d19018cce764dde0b5667aef68e40c41aa
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-06-25 17:50:54 +0800

    test: rebuild react to prepare ec2 version

--- server/models/databasePool.ts
+++ server/models/databasePool.ts
@@ -3,21 +3,25 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const localServerPath = 'C:/Users/sherr/OneDrive/桌面/AWS/mySandBox/server/';
-const local = process.env.SERVER_PATH === localServerPath;
+// const localServerPath = 'C:/Users/sherr/OneDrive/桌面/AWS/mySandBox/server/';
+// const local = process.env.SERVER_PATH === localServerPath;
 
-const host = local ? process.env.MYSQL_HOST : process.env.AWS_RDS_ENDPOINT;
-const user = local ? process.env.MYSQL_USER : process.env.AWS_RDS_USER;
-const password = local ? process.env.MYSQL_PASSWORD : process.env.AWS_RDS_KEY;
-const database = local
-  ? process.env.MYSQL_DATABASE
-  : process.env.AWS_RDS_DATABASE;
+// const host = local ? process.env.MYSQL_HOST : process.env.AWS_RDS_ENDPOINT;
+// const user = local ? process.env.MYSQL_USER : process.env.AWS_RDS_USER;
+// const password = local ? process.env.MYSQL_PASSWORD : process.env.AWS_RDS_KEY;
+// const database = local
+//   ? process.env.MYSQL_DATABASE
+//   : process.env.AWS_RDS_DATABASE;
 
 const pool = mysql.createPool({
-  host,
-  user,
-  password,
-  database,
+  // host,
+  // user,
+  // password,
+  // database,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 export default pool;
